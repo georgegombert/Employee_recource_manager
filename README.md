03a81889d16b4d79b2f8d17c74e06c0d65b4a11c
george-g <georgegombert@gmail.com>
2020-05-04 22:13:46 -0600

    moved functions to modules. updated readme

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-## Employee_recource_manager
+## Employee_resource_manager
   ![Version](https://img.shields.io/badge/Version-1.0-green)
   ****
   ### Description
-  This is a node project that takes in user input and generates an employee information page.
+  This is a node project that takes in user input and generates an employee information page based on that input. The user will be able to choose the type of employee on their team and set their information. The app is designed for small work teams that need an easy layout of information.
+
+  ### Photo
+![application](./app.png)
+
   ### Table of Contents
   [Installation](#Installation)
   [Usage](#Usage)
@@ -15,22 +19,26 @@
   ### Installation
   
   ```
-  
+  npm -i
 ```
   ### Usage
   
   ```
-  
+  node app.js
 ```
 
   ### License ![License](https://img.shields.io/badge/License-MIT-blue)
   
   ### Contributing
-  George GOmbert
+  George Gombert
   ### Tests
-  test with jest ''' npm test '''
+  test with jest 
+
+  ```
+  npm test 
+  ```
   ### Questions or Issues
-  If you have any questions or have found issues with the program, please reach out to Employee_recource_manager
-  <img src="https://avatars3.githubusercontent.com/u/59551782?v=4" alt="Github Avatar" width="200"/> [Email](null)
+  If you have any questions or have found issues with the program, please reach out to George
+  <img src="https://avatars3.githubusercontent.com/u/59551782?v=4" alt="Github Avatar" width="200"/> [Email](georgegombert@gmail.com)
   ### Project Status
-  in the works
+  Initial development released. Planned updates are to sharpen the styling of the output and add more functionality to the roster creation. Adding functionality to edit employee information and remove employees from the list is top of the list for that.
--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const returnScreen = require("./lib/returnScreen");
+const screenPrompts = require("./lib/screenPrompts");
+const addEmployee = require("./lib/addEmployee");
 const path = require("path");
 const fs = require("fs");
-const returnScreen = require("./lib/returnScreen");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -12,94 +14,82 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
-const welcomeScreen = () => {
-  return inquirer
-    .prompt([
-      {
-        type: "list",
-        message: "\n\nWelcome to your employee dashboard. \nThe following program will help you keep track of your employee roster and add new employees.\nHit 'Continue' to get started ",
-        choices: [{name:"Continue", value:true}, {name: "Cancel", value:false}],
-        name: "welcomeChoice"
-      }
-    ])
-}
-
-const homeScreen = () =>{
-  return inquirer
-    .prompt([
-      {
-        type: "list",
-        message: "Add Employee Home. What would you like to do?",
-        choices: [{name: "Add Manager", value:"manager"},{name: "Add Engineer", value:"engineer"},{name: "Add Intern", value:"intern"}],
-        name:"homeChoice"
-      }
-    ])
-}
-
-const baseEmployee = () =>{
-  return inquirer
-    .prompt([
-    {
-      type: "input",
-      message: "Enter employee name:",
-      name: "name"
-    },
-    {
-      type: "input",
-      message: "Enter employee id:",
-      name: "id"
-    },
-    {
-      type: "input",
-      message: "Enter employee email:",
-      name: "email"
-    }
-  ])
-}
-
-const addManager = () =>{
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "Enter employee office number:",
-        name: "officeNumber"
-      }
-  ])
-}
-
-const addEngineer = () =>{
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "Enter employee github username:",
-        name: "github"
-      }
-  ])
-}
-
-const addIntern = () =>{
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "Current school intern is attending:",
-        name: "school"
-      }
-  ])
-}
-
-// const returnScreen = () =>{
+// const welcomeScreen = () => {
 //   return inquirer
 //     .prompt([
 //       {
 //         type: "list",
-//         message: "What would you like to do now?",
-//         choices: [{name: "Add another employee", value: true}, {name: "Quit", value: false}],
-//         name: "returnChoice"
+//         message: "\n\nWelcome to your employee dashboard. \nThe following program will help you keep track of your employee roster and add new employees.\nHit 'Continue' to get started ",
+//         choices: [{name:"Continue", value:true}, {name: "Cancel", value:false}],
+//         name: "welcomeChoice"
 //       }
 //     ])
+// }
+
+// const homeScreen = () =>{
+//   return inquirer
+//     .prompt([
+//       {
+//         type: "list",
+//         message: "Add Employee Home. What would you like to do?",
+//         choices: [{name: "Add Manager", value:"manager"},{name: "Add Engineer", value:"engineer"},{name: "Add Intern", value:"intern"}],
+//         name:"homeChoice"
+//       }
+//     ])
+// }
+
+// const baseEmployee = () =>{
+//   return inquirer
+//     .prompt([
+//     {
+//       type: "input",
+//       message: "Enter employee name:",
+//       name: "name"
+//     },
+//     {
+//       type: "input",
+//       message: "Enter employee id:",
+//       name: "id"
+//     },
+//     {
+//       type: "input",
+//       message: "Enter employee email:",
+//       name: "email"
+//     }
+//   ])
+// }
+
+// const addManager = () =>{
+//   return inquirer
+//     .prompt([
+//       {
+//         type: "input",
+//         message: "Enter employee office number:",
+//         name: "officeNumber"
+//       }
+//   ])
+// }
+
+// const addEngineer = () =>{
+//   return inquirer
+//     .prompt([
+//       {
+//         type: "input",
+//         message: "Enter employee github username:",
+//         name: "github"
+//       }
+//   ])
+// }
+
+// const addIntern = () =>{
+//   return inquirer
+//     .prompt([
+//       {
+//         type: "input",
+//         message: "Current school intern is attending:",
+//         name: "school"
+//       }
+//   ])
 // }
 
 const employees = [];
@@ -107,24 +97,24 @@ const employees = [];
 async function init(){
   let returnChoice = true;
 
-  const {welcomeChoice} = await welcomeScreen();
+  const {welcomeChoice} = await screenPrompts.welcome();
   welcomeChoice? returnChoice = true : returnChoice = false;
   while (returnChoice === true){
-    welcomeChoice? {homeChoice} = await homeScreen() : console.log("signing off");
-    const employee = await baseEmployee();
+    welcomeChoice? {homeChoice} = await screenPrompts.home() : console.log("signing off");
+    const employee = await addEmployee.baseEmployee();
     switch(homeChoice){
       case "manager":
-        employee.officeNumber = (await addManager()).officeNumber;
+        employee.officeNumber = (await addEmployee.addManager()).officeNumber;
         const manager = new Manager(employee.name, employee.id, employee.email, employee.officeNumber);
         employees.push(manager);
         break;
       case "engineer":
-        employee.github = (await addEngineer()).github;
+        employee.github = (await addEmployee.addEngineer()).github;
         const engineer = new Engineer(employee.name, employee.id, employee.email, employee.github);
         employees.push(engineer);
         break;
       case "intern":
-        employee.school = (await addIntern()).school;
+        employee.school = (await addEmployee.addIntern()).school;
         const intern = new Intern(employee.name, employee.id, employee.email, employee.school);
         employees.push(intern);
         break;
@@ -140,40 +130,3 @@ async function init(){
 }
 
 init();
-
-
-
-
-// const chad = new Manager("chad", 1, "cdB@bnb.com", 6969);
-// const dave = new Manager("dave", 2, "dave@bnb.com", 420);
-// const mike = new Engineer("mike", 3, "mike@bnb.com", "georgegombert");
-// const tina = new Intern("tina", 4, "tina@bnb.com", "UCLA");
-
-// const employees = [chad, dave, mike, tina];
-
-// const index = render(employees);
-
-// fs.writeFile("./index.html",index, () => console.log("file created successfully"));
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
